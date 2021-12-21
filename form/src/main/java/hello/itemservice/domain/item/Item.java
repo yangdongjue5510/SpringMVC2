8153066f40358f196a5288216d804fecb7370fb4
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-21 21:40:01 +0900

    211221

---
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,28 @@
+package hello.itemservice.domain.item;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    private boolean open; //판매 여부
+    private List<String> regions; //등록지역
+    private ItemType itemType; //상품 종류
+    private String deliveryCode; //배송 방식
+
+   public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
