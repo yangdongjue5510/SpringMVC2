3e7ba94d35c360f251bf5058481255c3cea50044
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-23 21:04:36 +0900

    bindingResult

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -0,0 +1,102 @@
+package hello.itemservice.web.validation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/validation/v2/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV2 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v2/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v2/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            //bindingResult.addError(new FieldError("item", "itemName", "상품명이 없습니다."));
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품명없다."));
+        }
+        if (item.getPrice() == null || item.getPrice() > 10000000 || item.getPrice() < 1000) {
+            //bindingResult.addError(new FieldError("item", "price", "가격은 천원에서 백만원 사이를 허용합니다."));
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격문제."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            //bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9999개까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량문제."));
+        }
+
+        //특정 필드가 아닌 복합 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int result = item.getPrice() * item.getQuantity();
+            if (result < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "수량과 가격의 곱이 10000보다 커야 합니다."));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            //bindingResult는 자연스럽게 뷰로 전달되서 모델로 넣을 필요 없다.
+            return "validation/v2/addForm";
+        }
+
+        //성공로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+}
+
