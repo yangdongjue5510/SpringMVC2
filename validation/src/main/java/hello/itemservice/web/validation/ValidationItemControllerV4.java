deaaded3a05ecc9bec07873eafa389bbc90e179f
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-29 00:16:08 +0900

    Form 전송 객체 분리 - 프로젝트 준비 V4

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,98 @@
+package hello.itemservice.web.validation;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+	private final ItemRepository itemRepository;
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "validation/v4/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v4/item";
+	}
+
+	@GetMapping("/add")
+	public String addForm(Model model) {
+		model.addAttribute("item", new Item());
+		return "validation/v4/addForm";
+	}
+
+	@PostMapping("/add") //item을 검증후 bindingResult에 저장
+	public String addItem(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes, Model model) {
+		//특정 필드가 아닌 복합 검증
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int result = item.getPrice() * item.getQuantity();
+			if (result < 10000) {
+				//bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, result}, null));
+				bindingResult.reject("totalPriceMin", new Object[] {10000, result}, null);
+			}
+		}
+
+		if (bindingResult.hasErrors()) {
+			//bindingResult는 자연스럽게 뷰로 전달되서 모델로 넣을 필요 없다.
+			return "validation/v4/addForm";
+		}
+
+		//성공로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v4/items/{itemId}";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v4/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+		//특정 필드가 아닌 복합 검증
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int result = item.getPrice() * item.getQuantity();
+			if (result < 10000) {
+				//bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, result}, null));
+				bindingResult.reject("totalPriceMin", new Object[] {10000, result}, null);
+			}
+		}
+
+		if (bindingResult.hasErrors()) {
+			return "validation/v4/editForm";
+		}
+
+		itemRepository.update(itemId, item);
+		return "redirect:/validation/v4/items/{itemId}";
+	}
+}
